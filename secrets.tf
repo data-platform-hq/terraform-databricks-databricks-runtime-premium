2db5b75dd728c9d47701d5b5fcbca06cee649bae
dmytro_velychko3 <you@example.com>
2023-05-22 12:00:28 +0300

    fix: added databricks_secret_acl

--- secrets.tf
+++ secrets.tf
@@ -68,3 +68,11 @@ resource "databricks_secret_scope" "external" {
   }
   depends_on = [azurerm_key_vault_access_policy.databricks]
 }
+
+resource "databricks_secret_acl" "external" {
+  count = var.key_vault_secret_scope.key_vault_id != null ? 1 : 0
+
+  scope      = databricks_secret_scope.app.external[0].name
+  principal  = "users"
+  permission = "READ"
+}
