03d35c958168cde5d2f7bbce29a329701536753e
owlleg6 <owlleg6@gmail.com>
2023-07-18 19:39:46 +0300

    fix: removed duplicates

--- secrets.tf
+++ secrets.tf
@@ -61,23 +61,6 @@ resource "azurerm_key_vault_access_policy" "databricks" {
   ]
 }
 
-# Azure Key Vault-backed Scope
-resource "azurerm_key_vault_access_policy" "databricks" {
-  for_each = var.create_databricks_access_policy_to_key_vault ? {
-    for param in var.key_vault_secret_scope : (param.name) => param
-    if length(param.name) != 0
-  } : {}
-
-  key_vault_id = each.value.key_vault_id
-  object_id    = "9b38785a-6e08-4087-a0c4-20634343f21f" # Global 'AzureDatabricks' SP object id
-  tenant_id    = data.azurerm_key_vault_secret.tenant_id.value
-
-  secret_permissions = [
-    "Get",
-    "List",
-  ]
-}
-
 resource "databricks_secret_scope" "external" {
   for_each = {
     for param in var.key_vault_secret_scope : (param.name) => param
