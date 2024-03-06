f53bd9730bd04f7290013b6818fe4f41b1f7409f
Dima <velychkodmytro@gmail.com>
2024-03-07 01:08:14 +0200

    fix: support single node

--- cluster.tf
+++ cluster.tf
@@ -1,29 +1,47 @@
+locals {
+  spark_conf_single_node = {
+    "spark.master"                     = "local[*]"
+    "spark.databricks.cluster.profile" = "singleNode"
+  }
+  conf_passthrought = {
+    "spark.databricks.cluster.profile" : "serverless",
+    "spark.databricks.repl.allowedLanguages" : "python,sql",
+    "spark.databricks.passthrough.enabled" : "true",
+    "spark.databricks.pyspark.enableProcessIsolation" : "true"
+  }
+}
+
 resource "databricks_cluster" "cluster" {
   for_each = { for cluster in var.clusters : cluster.cluster_name => cluster }
 
   cluster_name  = each.value.cluster_name
   spark_version = each.value.spark_version
-  spark_conf = each.value.cluster_conf_passthrought ? merge({
-    "spark.databricks.cluster.profile" : "serverless",
-    "spark.databricks.repl.allowedLanguages" : "python,sql",
-    "spark.databricks.passthrough.enabled" : "true",
-    "spark.databricks.pyspark.enableProcessIsolation" : "true"
-  }, each.value.spark_conf) : each.value.spark_conf
+  spark_conf = merge(
+    each.value.cluster_conf_passthrought ? local.conf_passthrought : {},
+    each.value.single_node_enable == true ? local.spark_conf_single_node : {},
+  each.value.spark_conf)
   spark_env_vars          = each.value.spark_env_vars
   data_security_mode      = each.value.cluster_conf_passthrought ? null : each.value.data_security_mode
   node_type_id            = each.value.node_type_id
   autotermination_minutes = each.value.autotermination_minutes
   single_user_name        = each.value.single_user_name
+  custom_tags             = each.value.single_node_enable ? each.value.custom_tags : null
 
-  autoscale {
-    min_workers = each.value.min_workers
-    max_workers = each.value.max_workers
+  dynamic "azure_attributes" {
+    for_each = each.value.single_node_enable == true ? [] : [1]
+    content {
+      availability       = each.value.availability
+      first_on_demand    = each.value.first_on_demand
+      spot_bid_max_price = each.value.spot_bid_max_price
+    }
   }
 
-  azure_attributes {
-    availability       = each.value.availability
-    first_on_demand    = each.value.first_on_demand
-    spot_bid_max_price = each.value.spot_bid_max_price
+  dynamic "autoscale" {
+    for_each = each.value.single_node_enable == true ? [] : [1]
+    content {
+      min_workers = each.value.min_workers
+      max_workers = each.value.max_workers
+    }
   }
 
   dynamic "cluster_log_conf" {
